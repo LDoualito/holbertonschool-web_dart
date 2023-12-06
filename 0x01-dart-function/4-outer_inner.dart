009fc86c5e89b474bd7c02babc92a53ad314de49
LDoualito <3242@holbertonschool.com>
2023-12-06 14:08:33 +0100

    correct output

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -3,8 +3,9 @@
 void outer(String name, String id) {
   String inner() {
     List<String> nameParts = name.split(' ');
-    String lastName = nameParts.length > 1 ? nameParts[1][0] : '';
-    return 'Hello Agent ${nameParts[1]}.${nameParts[0]} your id is $id';
+    String lastName = nameParts.length > 1 ? nameParts[1] : '';
+    String formattedLastName = lastName.isNotEmpty ? '${lastName[0]}' : '';
+    return 'Hello Agent $formattedLastName.${nameParts[0]} your id is $id';
   }
 
   print(inner());
